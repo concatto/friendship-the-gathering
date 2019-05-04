3f96b30c76f427c1bf0e5c48ff073b036f21ec16
Fernando Concatto <fernandoconcatto@gmail.com>
2019-05-03 23:40:15 -0300

    Quase nada

--- app/src/components/Admin.jsx
+++ app/src/components/Admin.jsx
@@ -50,72 +50,78 @@ class Admin extends Component {
     ));
 
     return (
-      <Box>
-        <form onSubmit={e => this.submit(e)}>
-          <Box direction="column">
-            <TextField
-              value={values.name || ''}
-              onChange={e => this.setValue('name', e.target.value)}
-              label="Nome"
-              id="name"
-            />
+      <Box direction="row" width="100%" mainAlign="space-evenly">
+        <Box>
+          <form onSubmit={e => this.submit(e)}>
 
-            <Box padding="1 0" />
+            <Box direction="column">
+              <TextField
+                value={values.name || ''}
+                onChange={e => this.setValue('name', e.target.value)}
+                label="Nome"
+                id="name"
+              />
 
-            <TextField
-              value={values.email || ''}
-              onChange={e => this.setValue('email', e.target.value)}
-              label="E-mail"
-              type="email"
-              id="email"
-            />
+              <Box padding="1 0" />
 
-            <Box padding="2 0" />
+              <TextField
+                value={values.email || ''}
+                onChange={e => this.setValue('email', e.target.value)}
+                label="E-mail"
+                type="email"
+                id="email"
+              />
 
-            <FormControl>
-              <InputLabel htmlFor="social-group">Grupo social</InputLabel>
-              <Select
-                value={values.socialGroup}
-                onChange={e => this.setValue('socialGroup', e.target.value)}
-                inputProps={{
-                  name: 'socialGroup',
-                  id: 'social-group',
-                }}
-              >
-                {options}
-              </Select>
-            </FormControl>
+              <Box padding="2 0" />
 
-            <Box padding="3 0" />
+              <FormControl>
+                <InputLabel htmlFor="social-group">Grupo social</InputLabel>
+                <Select
+                  value={values.socialGroup}
+                  onChange={e => this.setValue('socialGroup', e.target.value)}
+                  inputProps={{
+                    name: 'socialGroup',
+                    id: 'social-group',
+                  }}
+                >
+                  {options}
+                </Select>
+              </FormControl>
 
-            <Button type="submit" color="primary" variant="contained">
+              <Box padding="3 0" />
+
+              <Button type="submit" color="primary" variant="contained">
               Registrar
-            </Button>
-          </Box>
-        </form>
+              </Button>
+            </Box>
+          </form>
+        </Box>
 
-        <Box padding="6 0" />
+        <Box>
+          <Typography variant="h4" gutterBottom>
+            Gerar e-mails
+          </Typography>
 
-        <Typography variant="h4" gutterBottom>
-          Gerar e-mails
-        </Typography>
-        <FormControl>
-          <InputLabel htmlFor="social-group-emails">Grupo social</InputLabel>
-          <Select
-            value={groupEmail}
-            onChange={e => this.setState({ groupEmail: e.target.value })}
-            inputProps={{
-              name: 'groupEmail',
-              id: 'social-group-emails',
-            }}
-          >
-            {options}
-          </Select>
-        </FormControl>
-        <Box padding="3 0" />
-        <Button color="secondary" variant="contained" onClick={() => this.generateEmails()}>
+          <Box padding="2 0" />
+
+          <FormControl>
+            <InputLabel htmlFor="social-group-emails">Grupo social</InputLabel>
+            <Select
+              value={groupEmail}
+              onChange={e => this.setState({ groupEmail: e.target.value })}
+              inputProps={{
+                name: 'groupEmail',
+                id: 'social-group-emails',
+              }}
+            >
+              {options}
+            </Select>
+          </FormControl>
+          <Box padding="3 0" />
+          <Button color="secondary" variant="contained" onClick={() => this.generateEmails()}>
           Gerar
-        </Button>
+          </Button>
+        </Box>
       </Box>
     );
   }

77f0b1d8f725be75f896f2e11804046dac2c5473
Fernando Concatto <fernandoconcatto@gmail.com>
2019-07-29 16:21:30 -0300

    TCLE adicionado

--- app/src/components/Welcome.jsx
+++ app/src/components/Welcome.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-  Typography, Button, CircularProgress, Grid, Link,
+  Typography, Button, CircularProgress, Grid, Link, FormControlLabel, Checkbox, withWidth, TextField,
 } from '@material-ui/core';
 import { Redirect, Link as RouterLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
@@ -8,6 +8,9 @@ import Box from './Box';
 import { withToken } from '../database';
 import { getToken } from '../utils';
 import ColouredSpan from './ColouredSpan';
+// import tcle from '../res/tcle.pdf';
+
+const tcle = 'https://concatto-ttc.s3-sa-east-1.amazonaws.com/tcle_meta.pdf';
 
 const Paragraph = ({ children, ...rest }) => (
   <Typography style={{ fontSize: 18, marginBottom: 22, fontWeight: 300 }} {...rest}>
@@ -23,7 +26,11 @@ class Welcome extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      checked: false,
+      phone: '',
+      name: '',
+    };
   }
 
   componentDidMount() {
@@ -38,6 +45,7 @@ class Welcome extends Component {
       this.setState({
         self: self.data(),
         remaining: remaining.length,
+        name: self.data().name,
       });
     });
   }
@@ -49,9 +57,16 @@ class Welcome extends Component {
     history.push(`/gather/${token}`);
   }
 
+  handleChange(name) {
+    return e => this.setState({ [name]: e.target.value });
+  }
+
   render() {
     console.log(this.props);
-    const { self, remaining } = this.state;
+    const { width } = this.props;
+    const {
+      self, remaining, checked, name, phone,
+    } = this.state;
 
     if (remaining === 0) {
       return (
@@ -63,9 +78,15 @@ class Welcome extends Component {
 
     return (
       <Box crossAlign="center">
-        <Typography variant="h2">
-          Friendship: The Gathering
-        </Typography>
+        <Box crossAlign="center">
+
+          <Typography variant="h2" align="center" gutterBottom>
+            Instrumento de Pesquisa:
+          </Typography>
+          <Typography variant="h4" align="center">
+            Proximidade Social no Ensino Superior
+          </Typography>
+        </Box>
 
         <Box padding="8 0 4" crossAlign="center">
           <Grid container justify="center">
@@ -77,21 +98,56 @@ class Welcome extends Component {
                 <span>Este é um questionário desenvolvido para coletar algumas informações </span>
                 <span>sobre seu círculo social, com o objetivo de viabilizar um estudo </span>
                 <span>multidisciplinar que busca compreender como <ColouredSpan colour="secondary">laços de amizade</ColouredSpan> </span>
-                <span>influenciam no desempenho de estudantes. Se quiser, você pode conferir </span>
-                <span>mais informações sobre o estudo e os pesquisadores responsáveis <Link component={InfoLink} color="primary">aqui</Link>.</span>
+                <span>influenciam no desempenho de estudantes. Para participar da pesquisa, você deve estar </span>
+                <span>de acordo com o Termo de Consentimento Livre e Esclarecido, apresentado abaixo.</span>
               </Paragraph>
-              <Paragraph>
-                <span>Sabemos que perguntas desta natureza são bastante sensíveis e </span>
-                <span>entendemos que elas podem lhe causar algum desconforto. <ColouredSpan colour="secondary">Sinta-se livre</ColouredSpan> </span>
-                <span>para não responder o questionário caso não se sentir confortável.</span>
-              </Paragraph>
-              <Paragraph>
-                <span>Preocupamo-nos muito com o sigilo de suas informações e tomamos o </span>
-                <span>máximo de cuidado para protegê-las. Todos os seus dados pessoais </span>
-                <span>passarão por um processo de <ColouredSpan colour="secondary">anonimização</ColouredSpan>, transformando em código </span>
-                <span>quaisquer informações que possam lhe identificar.</span>
-              </Paragraph>
-              <Paragraph color="primary">
+
+              <Box padding="2 0">
+                {/* <object data={tcle} type="application/pdf" width="100%" height="600px" name="test" title="second">
+                  <embed src={tcle} type="application/pdf" title="third" />
+                </object> */}
+                <embed
+                  src={`https://drive.google.com/viewerng/viewer?embedded=true&url=${tcle}`}
+                  width="100%"
+                  height="600"
+                />
+              </Box>
+
+              <Box crossAlign="center" padding="2 0 4">
+                <Button color="secondary" variant="outlined" component="a" href={tcle} target="_blank">
+                  Baixar via
+                </Button>
+              </Box>
+
+              <Box padding="0 0 6" crossAlign="center">
+                <Box padding={width === 'xs' ? '0' : '0 8'}>
+                  <Box direction="row" crossAlign="flex-start">
+
+                    <Checkbox
+                      color="primary"
+                      checked={checked}
+                      onChange={e => this.setState({ checked: e.target.checked })}
+                    />
+                    <Box padding="1 0 0 1">
+                      <Paragraph>
+                    Eu concordo em participar do presente estudo como participante. O pesquisador me
+                    informou sobre tudo o que vai acontecer na pesquisa, o que terei que fazer, inclusive sobre os
+                    possíveis riscos e benefícios envolvidos na minha participação. O pesquisador me garantiu que
+                    eu poderei sair da pesquisa a qualquer momento, sem dar nenhuma explicação, e que esta
+                    decisão não me trará nenhum tipo de penalidade ou interrupção de meu tratamento.
+                      </Paragraph>
+                    </Box>
+                  </Box>
+
+                  <Box padding={width === 'xs' ? '2 0' : '2 2'}>
+                    <TextField label="Nome" value={name} onChange={this.handleChange('name')} />
+                    <Box padding="1 0" />
+                    <TextField label="Telefone para contato" value={phone} onChange={this.handleChange('phone')} />
+                  </Box>
+                </Box>
+              </Box>
+
+              <Paragraph color="primary" align="center">
                 Ficamos muito gratos pela sua atenção e compreensão.
               </Paragraph>
             </Grid>
@@ -111,7 +167,7 @@ class Welcome extends Component {
                   </Typography>
                 </Box>
 
-                <Button variant="contained" color="primary" onClick={() => this.handleStart()} size="large">
+                <Button variant="contained" color="primary" onClick={() => this.handleStart()} size="large" disabled={!checked}>
                   Começar
                 </Button>
               </>
@@ -123,4 +179,4 @@ class Welcome extends Component {
   }
 }
 
-export default Welcome;
+export default withWidth()(Welcome);
